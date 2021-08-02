f85a1d4382fc19f46362070272f69976eec3e3f6
Alex <aleksandar.lokoski@gmail.com>
2021-08-02 10:30:28 -0400

    initial commit

---
+++ src/components/MainComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from './navbar/NavBar';
+import HomePage from './homepage/HomePage';
+import AboutUs from './aboutus/AboutUs';
+import Menu from './menu/Menu';
+import ContactUs from './contactus/ContactUs';
+
+const MainComponent = () => {
+    return (
+        <div className="main-container">
+            
+        </div>
+    )
+}
+
+export default MainComponent
