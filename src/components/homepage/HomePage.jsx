3a767fadfdba39c22207ab478b394cb4585f4fc2
Alex <aleksandar.lokoski@gmail.com>
2021-08-06 23:00:57 -0400

    finished landing

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -6,8 +6,10 @@ const HomePage = () => {
             <p className="description">A cozy, homey cafe featuring Balkan
              comfort cuisine & desserts with
               inviting indoor and outdoor seating.</p>
-              <div className="reso-border"><p>FOR PARTIES OF 9 OR MORE PLEASE CALL</p></div>
-              
+              <div className="reso-border">
+                <p>FOR PARTIES OF 9 OR MORE PLEASE CALL (973) 928-1371</p>
+                <p></p>
+                </div>
         </div>
     )
 }
