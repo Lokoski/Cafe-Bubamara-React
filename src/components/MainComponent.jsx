8c85d73367cc73cea0a22b0d84f208b965792212
Alex <aleksandar.lokoski@gmail.com>
2021-08-07 23:37:57 -0400

    gallery initial commit

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -4,14 +4,17 @@ import HomePage from './homepage/HomePage';
 import AboutUs from './aboutus/AboutUs';
 import Menu from './menu/Menu';
 import ContactUs from './contactus/ContactUs';
+import Gallery from './gallery/Gallery';
 import './navbar/NavBar.css'
-import './homepage/HomePage.css'
+import './homepage/HomePage.css';
+import './gallery/Gallery.css'
 
 const MainComponent = () => {
     return (
         <div className="main-container">
             <NavBar />
             <HomePage />
+            <Gallery />
         </div>
     )
 }
---
+++ src/components/gallery/Gallery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="gallery-container">
+      <h1 className="gallery-title">Welcome to Cafe Bubamara</h1>
+      <p className="gallery-paragraph">
+        Our goal is good food, good people, and good times! <br /> Join us in sharing
+        an experience and over a great meal.
+      </p>
+    </div>
+  );
+};
+
+export default Gallery;
