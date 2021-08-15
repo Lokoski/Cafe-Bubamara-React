12a0593c398041af426488edd67706a29ed6db25
Alex <aleksandar.lokoski@gmail.com>
2021-08-14 23:44:47 -0400

    media init

--- src/components/media/Media.jsx
+++ src/components/media/Media.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "./Carousel";
-import newspaper from '../../assets/images/Bubamara_Article.png'
+import newspaper from "../../assets/images/Bubamara_Article.png";
 
 const Media = () => {
   return (
@@ -8,7 +8,7 @@ const Media = () => {
       <h1>Media & Articles</h1>
       <div className="media">
         <div className="media-image">
-            <img src={newspaper} alt="" />
+          <img src={newspaper} alt="" />
         </div>
         <div className="media-carousal">
           <Carousel />
