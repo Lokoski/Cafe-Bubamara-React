7223d5a97a52b632470539408721f19d32cdab84
Alex <aleksandar.lokoski@gmail.com>
2021-08-15 15:32:09 -0400

    carousel fixed

--- src/components/newestitems/ItemsCard.jsx
+++ src/components/newestitems/ItemsCard.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const ItemsCard = ({image, title, description}) => {
-    console.log(image)
+const ItemsCard = ({ image, title, description }) => {
+  console.log(image);
 
-    return (
-        <div className="items-card">
-            <img alt="" src={image} />
-            <div className="items-description">
-            <p className="items-title">{title}</p>
-            <p>{description}</p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="items-card">
+      <div className="items-image">
+        <img alt="" src={image} />
+      </div>
+      <div className="items-description">
+        <p className="items-title">{title}</p>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
-export default ItemsCard
+export default ItemsCard;
